f8bf95892640d842c60ba2cb4c7b44e5b7dbe622
Prahlad Singh <prahladsingh7512@gmail.com>
2026-07-05 01:26:07 +0530

    Add leaderboard routes to progressRoutes

--- backend/routes/progressRoutes.js
+++ backend/routes/progressRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { completeLevel, getSummary } = require("../controllers/progressController");
+const {
+  completeLevel,
+  getSummary,
+  getLevelLeaderboard,
+  getGlobalLeaderboard,
+} = require("../controllers/progressController");
 const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/summary", protect, getSummary);
+router.get("/leaderboard", protect, getGlobalLeaderboard);
+router.get("/leaderboard/:levelNumber", protect, getLevelLeaderboard);
 router.post("/:levelNumber/complete", protect, completeLevel);
 
 module.exports = router;
