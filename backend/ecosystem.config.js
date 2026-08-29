24c9442727a33cf6c0b9778bffdf62b36ceab730
Prahlad Singh <prahladsingh7512@gmail.com>
2026-08-30 00:14:48 +0530

    Update working directory for ziprouter-api

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: 'ziprouter-api',
       script: 'server.js',
-      cwd: '/home/prahlad/ziprouter/backend',
+      cwd: '/var/www/projects/ziprouter/backend',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
       env_production: {
         NODE_ENV: 'production',
+        PORT: 5002,
       },
     },
   ],
